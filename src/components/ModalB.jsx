3a9ed782cd80a7ed6535fbe997b50fc6fa21d054
mamun-mahmood <mamamun1999@gmail.com>
2023-12-12 19:53:53 +0600

    done: Problem-2

--- src/components/ModalB.jsx
+++ src/components/ModalB.jsx
@@ -3,24 +3,28 @@ import ModalC from "./ModaC";
 import { useNavigate } from "react-router-dom";
 function ModalB() {
   const [contacts, setContacts] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
-  const [page, setPage] = useState(1);
-  const [pageSize, setPageSize] = useState(10);
   const loadingRef = useRef(null);
   const [isVisible, setIsVisible] = useState(false);
+  const [page, setPage] = useState(1);
+  const pageSize = 10;
+  const [fetchUrl, setFetchUrl] = useState(null);
+  const [fetchMore, setFetchMore] = useState(true);
+  const inputRef = useRef(null);
   const fetchContacts = () => {
-    fetch(
-      `https://contact.mediusware.com/api/country-contacts/usa?page=${page}&search=${searchTerm}&page_size=${pageSize}`
-    )
+    fetch(`https://contact.mediusware.com/api/country-contacts/bangladesh/`)
       .then((res) => res.json())
       .then((data) => {
-        setContacts(data.data);
+        console.log(data);
+        setContacts((prev) => [...prev, ...data.results]);
+        setPage((prev) => prev + 1);
       })
       .catch((err) => console.log(err.message));
   };
   useEffect(() => {
     fetchContacts();
-    // Create the Intersection Observer
+  }, [pageSize]);
+  useEffect(() => {
+    // Intersection Observer for infinite scrolling
     const options = {
       root: null,
       rootMargin: "0px",
@@ -30,8 +34,10 @@ function ModalB() {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           setIsVisible(true);
-          setPage((prev) => prev + 1);
-          fetchContacts();
+          // fecth more search results if fetchUrl is set
+          if (fetchUrl && fetchMore) {
+            fetchMoreSearchResults();
+          } else if (fetchMore) fetchContacts();
           observer.unobserve(entry.target);
         }
       });
@@ -44,23 +50,46 @@ function ModalB() {
     return () => {
       observer.disconnect();
     };
-  }, []);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    fetch(
-      `https://contact.mediusware.com/api/country-contacts/usa?page=${page}&search=${searchTerm}&page_size=${pageSize}`
+  }, [contacts]);
+  
+  const searchContacts = async () => {
+    await fetch(
+      `https://contact.mediusware.com/api/country-contacts/bangladesh/?search=${inputRef?.current.value}`
     )
       .then((res) => res.json())
       .then((data) => {
-        setContacts(data.data);
+        setContacts(data.results);
+        if (data?.next) {
+          setFetchUrl(data.next);
+          setFetchMore(true);
+        }
+        console.log(data);
       })
       .catch((err) => console.log(err.message));
   };
+  const fetchMoreSearchResults = async () => {
+    await fetch(fetchUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setContacts((prev) => [...prev, ...data.results]);
+        if (data?.next) {
+          setFetchUrl(data.next);
+        } else {
+          setFetchUrl(null);
+          setFetchMore(false);
+        }
+      })
+      .catch((err) => console.log(err.message));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputRef?.current?.value) searchContacts();
+    else fetchContacts();
+  };
   const handleChange = (e) => {
-    setSearchTerm(e.target.value);
     setTimeout(() => {
-      handleSubmit(e);
-    }, 1000);
+      searchContacts();
+    }, 2000);
   };
   const navigate = useNavigate();
   const [showEven, setShowEven] = useState(false);
@@ -81,7 +110,7 @@ function ModalB() {
           zIndex: 10,
         }}
       >
-        <h2>Modal B</h2>
+        <h2>Modal A</h2>
         <div className="d-flex justify-content-center gap-3">
           <button
             className="btn btn-lg btn-outline"
@@ -93,6 +122,7 @@ function ModalB() {
             All Contacts
           </button>
           <button
+            
             className="btn btn-lg btn-outline"
             style={{
               backgroundColor: "#ff7f50",
@@ -114,13 +144,14 @@ function ModalB() {
           style={{
             display: "flex",
             flexDirection: "column",
-            justifyContent: "center",
+            // justifyContent: "center",
             alignItems: "center",
             marginTop: "5px",
           }}
           onSubmit={handleSubmit}
         >
           <input
+            ref={inputRef}
             type="text"
             placeholder="Search"
             style={{
@@ -130,9 +161,9 @@ function ModalB() {
               padding: "5px",
             }}
             onChange={handleChange}
-            defaultValue={searchTerm}
           />
           <button
+            disabled={!inputRef?.current?.value}
             className="btn btn-sm btn-primary"
             type="submit"
             style={{ marginTop: "5px" }}
@@ -143,11 +174,14 @@ function ModalB() {
         <ul
           style={{
             marginTop: "5px",
+            overflowY: "scroll",
+            height: "60%",
+            paddingBottom: "20px",
           }}
         >
           {/* Map through contacts and display them */}
           {!showEven
-            ? contacts.map((contact) => (
+            ? contacts?.map((contact) => (
                 <ModalC contact={contact} key={contact.id} />
               ))
             : contacts
@@ -155,9 +189,10 @@ function ModalB() {
                 .map((contact) => (
                   <ModalC contact={contact} key={contact.id} />
                 ))}
-          {contacts.length > 10 && (
+
+          {contacts.length > 19 && (
             <p ref={loadingRef}>
-              {isVisible ? "Loading more data..." : "Scroll to load more"}
+              {fetchMore ? "Loading more data..." : "No more data to load"}
             </p>
           )}
         </ul>
